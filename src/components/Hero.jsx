829323f90d4770197e1e8564789fc120abc443c0
unknown <vandersontavares32@gmail.com>
2023-05-13 13:57:57 -0300

    hero margin update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
   return (
     <section className="hero">
       <div className="h-screen">
-      <div className="container mx-auto max-w-6xl text-center flex flex-col items-center justify-center mt-20 lg:mt-36">
+      <div className="container mx-auto max-w-6xl text-center flex flex-col items-center justify-center mt-14 lg:mt-36">
         <h1 className="text-5xl font-dance text-[#88521a]">Corte e qualidade</h1>
         <h2 className="text-[#729476] font-bold text-8xl uppercase mt-[-20px]">since 1997</h2>
         <p className="text-white max-w-2xl mt-10"> 
